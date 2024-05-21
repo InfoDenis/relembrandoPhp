50e6e63f148414920e975cc527ade67d40e86894
InfoDenis <112553151+InfoDenis@users.noreply.github.com>
2024-05-21 15:14:16 -0300

    Erro de upload

--- json/exemplo-02.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$json = '[{"nome":"Jo\u00e3o","idade":20},{"nome":"Glauco","idade":25}]';
-
-$data = json_decode($json, true);//Se tirar o valor true terá um objeto ao invés de um array
-
-var_dump($data);
