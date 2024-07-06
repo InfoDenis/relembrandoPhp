226ff04f41759f9271a33048969b28e8412b2c8f
InfoDenis <mensagemparavoce_54@hotmail.com>
2024-07-06 17:58:34 -0300

    Aula46-Instalando e configurando composer

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'infodenis/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd648e925593269f6d1b07d9182f245f698129661',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'infodenis/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd648e925593269f6d1b07d9182f245f698129661',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
