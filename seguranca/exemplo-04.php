2b5362e07b20c7feac0e8fb3d5e149c5802225da
InfoDenis <mensagemparavoce_54@hotmail.com>
2024-07-23 22:51:48 -0300

    Aula53-ReCaptcha Google

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<script src="https://www.google.com/recaptcha/api.js"></script>
+
+<script>
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+</script>
+
+<form action="cadastrar.php" method="POST" id="demo-form">
+
+  <input type="email" name="inputEmail">
+  <button class="g-recaptcha" 
+        data-sitekey="6LfbAxcqAAAAAAKAj0qLkCC679wfxuPzqizQKz2q" 
+        data-callback='onSubmit' 
+        data-action='submit'>Submit</button>
+
+</form>
